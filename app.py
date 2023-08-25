b952301af835743352ab455660675873ab67465d
boshify <140970186+boshify@users.noreply.github.com>
2023-08-25 00:15:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,37 @@
+from flask import Flask, render_template, request
 import pandas as pd
-from flask import Flask, render_template, request, jsonify
+import json
 
 app = Flask(__name__)
-latest_hierarchy = None  # global variable to store the latest processed data
+
+def insert_data(hierarchy, cluster, subcluster, keyword, volume, url, type):
+    if cluster not in hierarchy:
+        hierarchy[cluster] = {"name": cluster, "children": [], "type": "Existing", "value": 0}
+    if subcluster:
+        subclusters = [child for child in hierarchy[cluster]["children"] if child["name"] == subcluster]
+        if not subclusters:
+            hierarchy[cluster]["children"].append({"name": subcluster, "children": [], "type": "Existing", "value": 0})
+            subclusters = [child for child in hierarchy[cluster]["children"] if child["name"] == subcluster]
+        subclusters[0]["children"].append({"name": keyword, "value": volume, "type": type, "url": url})
+        subclusters[0]["value"] += volume
+    else:
+        hierarchy[cluster]["children"].append({"name": keyword, "value": volume, "type": type, "url": url})
+    hierarchy[cluster]["value"] += volume
+
+def process_file(file):
+    hierarchy = {}
+    df = pd.read_csv(file)
+    for _, row in df.iterrows():
+        insert_data(hierarchy, row['Cluster'], row['Subcluster'], row['Page Title'], row['Volume'], row['URL'], row['Type'])
+    return {"name": "Root", "children": [value for key, value in hierarchy.items()], "value": sum([value["value"] for key, value in hierarchy.items()])}
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global latest_hierarchy
     if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            latest_hierarchy = process_file(file)
-    return render_template('index.html')
+        file = request.files.get('file')
+        latest_hierarchy = process_file(file)
+        return render_template('index.html', hierarchy=json.dumps(latest_hierarchy))
+    return render_template('index.html', hierarchy=None)
 
-@app.route('/get-data', methods=['GET'])
-def get_data():
-    global latest_hierarchy
-    return jsonify(latest_hierarchy)
-
-def process_file(file):
-    df = pd.read_csv(file)
-    hierarchy = {
-        "name": "Root",
-        "children": []
-    }
-    for _, row in df.iterrows():
-        insert_data(hierarchy, row['Cluster 1'], row['Cluster 2'], row['Keyword'], row['Volume'], row['URL'], row['New/Existing'])
-    assign_cumulative_values(hierarchy)
-    return hierarchy
-
-def insert_data(hierarchy, cluster1, cluster2, keyword, volume, url, type):
-    cluster1_node = next((item for item in hierarchy["children"] if item["name"] == cluster1), None)
-    if not cluster1_node:
-        cluster1_node = {
-            "name": cluster1,
-            "children": [],
-            "type": type
-        }
-        hierarchy["children"].append(cluster1_node)
-
-    cluster2_node = next((item for item in cluster1_node["children"] if item["name"] == cluster2), None)
-    if not cluster2_node:
-        cluster2_node = {
-            "name": cluster2,
-            "children": [],
-            "type": type
-        }
-        cluster1_node["children"].append(cluster2_node)
-
-    keyword_node = {
-        "name": keyword,
-        "value": volume,
-        "url": url,
-        "type": type
-    }
-    cluster2_node["children"].append(keyword_node)
-
-def assign_cumulative_values(node):
-    if "children" not in node:
-        return node["value"]
-    else:
-        sum_values = 0
-        for child in node["children"]:
-            sum_values += assign_cumulative_values(child)
-        node["value"] = sum_values
-        return sum_values
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True)
