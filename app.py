0a76bb8bf49362171ed370867b0b682e591a35db
boshify <140970186+boshify@users.noreply.github.com>
2023-08-24 23:35:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import pandas as pd
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    data = None
+    js_code = ""
     if request.method == 'POST':
         file = request.files['file']
         if file:
-            data = process_file(file)
-    return render_template('index.html', data=data)
+            df = pd.read_csv(file)
+            hierarchy_data = process_csv_to_hierarchy(df)
+            js_code = generate_js_code(hierarchy_data)
+    return render_template('index.html', js_code=js_code)
 
-def process_file(file):
-    df = pd.read_csv(file)
-
+def process_csv_to_hierarchy(df):
     hierarchy = {'name': 'Root', 'children': []}
 
     for _, row in df.iterrows():
@@ -50,6 +50,45 @@ def assign_cumulative_values(node):
         return total_value
     else:
         return node.get('value', 1)
+
+def generate_js_code(hierarchy):
+    js_code_cumulative = f'''
+    am4core.ready(function() {{
+        var chart = am4core.create("chartdiv", am4plugins_forceDirected.ForceDirectedTree);
+        chart.zoomable = true;
+        var series = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries());
+        series.data = {hierarchy};
+        series.dataFields.value = "value";
+        series.dataFields.name = "name";
+        series.dataFields.children = "children";
+        series.dataFields.collapsed = "off";
+        series.nodes.template.tooltipText = "{{{{name}}}}: [bold]{{{{value}}}}[/]\\nURL: {{{{url}}}}";
+        series.nodes.template.label.text = "{{{{name}}}}";
+        series.nodes.template.label.fill = am4core.color("#000");
+        series.nodes.template.label.stroke = am4core.color("#FFF");
+        series.nodes.template.label.strokeWidth = 0.8;
+        series.fontSize = 14;
+        series.maxLevels = 2;
+        series.minRadius = am4core.percent(0.012);
+        series.nodePadding = 5;
+        series.manyBodyStrength = -30;
+        series.nodes.template.label.hideOversized = false;
+        series.nodes.template.label.truncate = false;
+        series.nodes.template.events.on("inited", function(event) {{
+            var node = event.target;
+            if (node.dataItem && node.dataItem.dataContext && node.dataItem.dataContext.type == "New") {{
+                var shadow = new am4core.DropShadowFilter();
+                shadow.dx = 0;
+                shadow.dy = 0;
+                shadow.blur = 10;
+                shadow.color = am4core.color("#00FF00");
+                shadow.opacity = 0.8;
+                node.filters.push(shadow);
+            }}
+        }});
+    }});
+    '''
+    return js_code_cumulative
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
